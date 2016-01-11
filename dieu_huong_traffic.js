ddf3c145df8c5530a1be871c6fd520383fe43ad8
LeeBridal <leminhtrongle@gmail.com>
2016-01-11 14:27:12 +0700

    Update dieu_huong_traffic.js

--- dieu_huong_traffic.js
+++ dieu_huong_traffic.js
@@ -1,1 +1,17 @@
-<script src='https://jgoogle.googlecode.com/svn/trunk/addlinkcopy.js' type='text/javascript'></script> 
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://aocuoisi.net'>http://aocuoisi.net"+document.location.pathname+"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
